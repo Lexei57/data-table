b1991bd4e7a9fc723b4fd93d60ed8431a6ee365a
Lexei57 <mnzllexei@gmail.com>
2023-03-28 21:14:29 +0300

    Refactored types for get request

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,36 +1,40 @@
-// export interface ITableUser {
-//   picture: string
-//   name: {
-//     first: string
-//     last: string
-//   }
-//   gender: string
-//   phone: number
-// }
-
-
+type Picture = {
+  large: string;
+  medium: string;
+  thumbnail: string;
+}
+type Name = {
+  first: string;
+  last: string;
+}
+type Street = {
+  number: number
+  name: string
+}
+type Location = {
+  city: string
+  street: Street
+}
+type FullName = string;
+type Gender = string;
+type Phone = string;
 
 export interface IUser {
-    picture: string
-    name: string
-    gender: string
-    phone: number
+    picture: Picture;
+    name: Name;
+    gender: Gender;
+    phone: Phone;
+    email: string
+    street: string
+    city: string
+    fullName: FullName;
+    location: Location
 }
+
 
 export interface IUserResponse {
   results: IUser[]
 }
 
-export interface IUserValues {
-  picture: {
-    medium: string
-  }
-  name: {
-    first: string
-    last: string
-  }
-  gender: string
-  phone: number
-}
 
 
